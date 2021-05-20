87d9b9b119eae7ab5e9f8cd089e4aea16b0b00dc
Joaquin Villanueva <joaquinvillanuevafarber@gmail.com>
2021-05-20 17:17:31 -0300

    sliders on :+1:

--- script.js
+++ script.js
@@ -1,52 +1,39 @@
-const winter = document.getElementById("winter")
-const summer = document.getElementById("summer")
+const winter = document.getElementById("winter");
+const winterContainer = document.getElementById("winter-container");
+const summer = document.getElementById("summer");
+const summerContainer= document.getElementById("summer-container");
 
-function openMenu(){
-  element = document.getElementById("catalogo");
-  menu = document.getElementsByClassName("catalogo-menu");
-  if (element.click){
-    menu.style.display="flex";
-  }
-  else{
-    menu.style.display="none";
-  }
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";
 }
 
 function showSummer(){
-  winter.style.color = "#5e4f4f";
+  winter.style.color = "#8F7B7B";
   summer.style.color = "#be8496";
+  summerContainer.style.display = "block";
+  winterContainer.style.display= "none";
 }
 
 function showWinter(){
-  summer.style.color = "#5e4f4f";
+  summer.style.color = "#8F7B7B";
   winter.style.color = "#8b8bd6";
+  summerContainer.style.display = "none";
+  winterContainer.style.display= "block";
 }
